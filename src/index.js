018e5ebcdc2eddc111d0e04e04efab3dd9851144
nguyen nha <nguyenphucnha111@gmail.com>
2021-04-10 11:25:12 +0700

    asd

--- src/index.js
+++ src/index.js
@@ -1,23 +1,3 @@
 module.exports = async function App(context) {
-  await context.sendGenericTemplate([
-    {
-      title: "Welcome to shop",
-      imageUrl: 'https://shop-ecommerces.herokuapp.com/images/promo/macbook-new.jpg',
-      subtitle: "Shop",
-      defaultAction: {
-        type: 'web_url',
-        url: 'https://shop-ecommerces.herokuapp.com/',
-        messengerExtensions: true,
-        webviewHeightRatio: 'tall',
-        fallbackUrl: 'https://shop-ecommerces.herokuapp.com/',
-      },
-      buttons: [
-        {
-          type: 'postback',
-          title: 'Start Chatting',
-          payload: 'DEVELOPER_DEFINED_PAYLOAD',
-        },
-      ],
-    },
-  ]);
+  await context.sendText('Welcome to shop');
 };
