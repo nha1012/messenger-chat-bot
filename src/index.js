5765d5bc2b343e765b29051308ca06a42639b1e3
nguyen nha <nguyenphucnha111@gmail.com>
2021-04-10 11:20:12 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = async function App(context) {
-  await context.sendText('Welcome to Bottender');
+  await context.sendGenericTemplate([
+    {
+      title: "Welcome to shop",
+      imageUrl: 'https://shop-ecommerces.herokuapp.com/images/promo/macbook-new.jpg',
+      subtitle: "Shop",
+      defaultAction: {
+        type: 'web_url',
+        url: 'https://shop-ecommerces.herokuapp.com/',
+        messengerExtensions: true,
+        webviewHeightRatio: 'tall',
+        fallbackUrl: 'https://shop-ecommerces.herokuapp.com/',
+      },
+      buttons: [
+        {
+          type: 'postback',
+          title: 'Start Chatting',
+          payload: 'DEVELOPER_DEFINED_PAYLOAD',
+        },
+      ],
+    },
+  ]);
 };
